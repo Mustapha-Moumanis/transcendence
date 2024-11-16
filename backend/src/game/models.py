93dde5582ca98020b0f1123bbb36f6e5a1ba0866
Mustapha Moumanis <mmoumani@e2r5p12.1337.ma>
2024-11-16 22:45:52 +0100

    add tournament model

--- backend/src/game/models.py
+++ backend/src/game/models.py
@@ -1,5 +1,6 @@
+import uuid
 from django.db import models
-from users.models import User
+from users.models import User, GenerateDefaultImagePath
 
 class LocalGame(models.Model):
     user = models.ForeignKey(User, related_name='local_games', on_delete=models.CASCADE)
@@ -11,3 +12,53 @@ class LocalGame(models.Model):
 
     def __str__(self):
         return f"Game: {self.user.username} vs {self.opponent}"
+
+# Tournament
+
+class Player(models.Model): 
+    name = models.CharField(max_length=100)
+    avatar = models.CharField(max_length=200, default=GenerateDefaultImagePath)
+
+    def __str__(self):
+        return self.name
+
+class Match(models.Model):
+    player1 = models.ForeignKey(Player, related_name="match_player1", on_delete=models.CASCADE)
+    player2 = models.ForeignKey(Player, related_name="match_player2", on_delete=models.CASCADE)
+    winner = models.ForeignKey(Player, related_name="match_winner", null=True, blank=True, on_delete=models.SET_NULL)
+    player1_score = models.IntegerField(default=0)
+    player2_score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.player1} vs {self.player2} - Winner {self.winner}"
+
+class LocalTournamentManager(models.Manager):
+
+    def tournament_list(self, user):
+        """ Return a list of all tournaments """
+
+        tournaments = self.filter(user=user)
+
+        tournament_list = list(tournaments)
+
+        return tournament_list
+
+
+    def add_player(self, data):
+        playersList = []
+        for player in data:
+            playersList.append(Player.objects.create(name=player.name, score=player.score))
+        print(">><< ", playersList)
+        return playersList
+
+class LocalTournament(models.Model):
+    key = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    user = models.ForeignKey(User, related_name='local_tournament', on_delete=models.CASCADE)
+    players = models.ManyToManyField(Player)
+    champion = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True, related_name='tournament_champion')
+    matchs = models.ManyToManyField(Match)
+
+    objects = LocalTournamentManager()
+
+    def __str__(self):
+        return f"Tournament key : {self.key}"
--- backend/src/game/serializers.py
+++ backend/src/game/serializers.py
@@ -1,20 +1,145 @@
 from rest_framework import serializers
-from .models import LocalGame
+from .models import LocalGame, Player, Match, LocalTournament
 # from users.serializers import ProfilesSerializer
 from django.core.validators import URLValidator, ValidationError
 
+import re
+import uuid
+
 class LocalGameSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = LocalGame
-        fields = ['nickname', 'opponent', 'user_score', 'opponent_score', 'result']
-        read_only_fields = ['user', 'result']
+	class Meta:
+		model = LocalGame
+		fields = ['nickname', 'opponent', 'user_score', 'opponent_score', 'result']
+		read_only_fields = ['user', 'result']
 
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
+	# def to_representation(self, instance):
+	#     representation = super().to_representation(instance)
 
-    #     if instance.user_score > instance.opponent_score:
-    #         representation['result'] = "Win"
-    #     elif instance.user_score < instance.opponent_score:
-    #         representation['result'] = "Loss"
-    #     return representation
+	#     if instance.user_score > instance.opponent_score:
+	#         representation['result'] = "Win"
+	#     elif instance.user_score < instance.opponent_score:
+	#         representation['result'] = "Loss"
+	#     return representation
+
+# class MatchSerializer(serializers.Serializer):
+# 	player1 = serializers.CharField(max_length=100)
+# 	player2 = serializers.CharField(max_length=100)
+# 	winner = serializers.CharField(max_length=100)
+# 	player1_score = serializers.IntegerField()
+# 	player2_score = serializers.IntegerField()
+
+# 	class Meta:
+# 		model = Match
+
+# class BracketDataSerializer(serializers.Serializer):
+# 	# bracket01 = MatchSerializer()
+# 	# bracket02 = MatchSerializer()
+# 	# bracketFinal = MatchSerializer()
+
+# 	class Meta:
+# 		model = LocalTournament
+
+class PlayersDataSerializer(serializers.Serializer):
+	player1 = serializers.CharField(max_length=100)
+	player2 = serializers.CharField(max_length=100)
+	player3 = serializers.CharField(max_length=100)
+	player4 = serializers.CharField(max_length=100)
+
+	def validate_name(self, name, player_name):
+		pattern = r'^[a-zA-Z][a-zA-Z_-]{0,19}$'
+		if not re.match(pattern, name):
+			raise serializers.ValidationError(f'Invalid {player_name} name')
+		return name
+
+	def validate_player1(self, value):
+		return self.validate_name(value, "player1")
+
+	def validate_player2(self, value):
+		return self.validate_name(value, "player2")
+
+	def validate_player3(self, value):
+		return self.validate_name(value, "player3")
+
+	def validate_player4(self, value):
+		return self.validate_name(value, "player4")
+
+	def validate(self, attrs):
+		if len(attrs.values()) != len(set(attrs.values())):
+			raise serializers.ValidationError({"detail": "Duplicate player names are not allowed."})
+
+		return attrs
+
+class PlayerSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Player
+		fields = ['name', 'avatar']
+
+class MatchSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Match
+		fields = ['player1', 'player2', 'winner', 'player1_score', 'player2_score']
+
+
+class LocalTournamentSerializer(serializers.ModelSerializer):
+	players = PlayerSerializer(many=True)
+	champion = PlayerSerializer()
+	matchs = MatchSerializer(many=True)
+
+	class Meta:
+		model = LocalTournament
+		fields = ['champion', 'players', 'matchs'] 
+
+class bracketSerializer(serializers.Serializer):
+	player1 = serializers.CharField(max_length=100)
+	player2 = serializers.CharField(max_length=100)
+	player1_score = serializers.IntegerField()
+	player2_score = serializers.IntegerField()
+
+	def validate_name(self, name, player_name):
+		pattern = r'^[a-zA-Z][a-zA-Z_-]{0,19}$'
+		if not re.match(pattern, name):
+			raise serializers.ValidationError(f'Invalid {player_name} name')
+		return name
+
+	def validate_player1(self, value):
+		return self.validate_name(value, "player1")
+
+	def validate_player2(self, value):
+		return self.validate_name(value, "player2")
+
+class EventsTournamentSerializer(serializers.ModelSerializer):
+	# key = serializers.IntegerField()
+	key = serializers.UUIDField(
+		default=uuid.uuid4, 
+		error_messages={
+			"invalid": "Please ensure it follows the correct format."
+		})
+
+	bracket01 = bracketSerializer()
+	bracket02 = bracketSerializer()
+	bracketFinal = bracketSerializer()
+
+	class Meta:
+		model = LocalTournament
+		fields = ['key', 'bracket01', 'bracket02', 'bracketFinal'] 
+
+	# def validate(self, attrs):
+	# 	players = [
+	# 		attrs['bracket01']['player1'],
+	# 		attrs['bracket01']['player2'],
+	# 		attrs['bracket02']['player1'],
+	# 		attrs['bracket02']['player2'],
+	# 		attrs['bracketFinal']['player1'],
+	# 		attrs['bracketFinal']['player2'],
+	# 	]
+
+	# 	pattern = r'^[a-zA-Z][a-zA-Z_-]{0,19}$'
+	# 	for player in players:
+	# 		if not re.match(pattern, player):
+	# 			raise serializers.ValidationError({"detail": f"Invalid player name: {player}"})
+
+	# 	if len(players) != len(set(players)):
+	# 		raise serializers.ValidationError({"detail": "Duplicate player names are not allowed."})
+
+	# 	return attrs
