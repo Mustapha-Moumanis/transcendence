7f73a942e4a243aee2048ef866552476d1954d66
Mustapha Moumanis <mmoumani@e1r1p7.1337.ma>
2024-11-24 15:25:30 +0100

    add /api to all backend url

--- backend/src/twoFactorAuth/urls.py
+++ backend/src/twoFactorAuth/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .router import router as TwoFactorAuth_router
 
 urlpatterns = [
-    path('2fa/', include(TwoFactorAuth_router.urls)), 
+    path('api/2fa/', include(TwoFactorAuth_router.urls)), 
 ]
