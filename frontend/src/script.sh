acb5986baba5ba577f41e6055e848f6d2375548c
Souad Hilal <shilal@c4r9p2.1337.ma>
2024-12-03 14:02:22 +0100

    update game Bracket & chat | update devops part

--- frontend/src/script.sh
+++ frontend/src/script.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# openssl req -x509 -nodes -newkey rsa:2048 \
-# -keyout $KEY -out $CRT -subj '/CN='$DOMAIN_NAME''
-
-# sed -i -e "s@Default_crt@'$CRT'@" \
-# -e "s@Default_key@'$KEY'@" \
-# -e "s@Default_name@'$DOMAIN_NAME'@" \
-# /etc/nginx/sites-available/default
-
-# CERTS_="/etc/ssl/certs/certificate.crt"
-# P_KEY="/etc/ssl/private/private.key"
-
 if ! [ -f "$CERTS_" ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout "$P_KEY" \
@@ -21,15 +10,6 @@ if ! [ -f "$CERTS_" ]; then
 else
     echo "There is already a certificate and private key!"
 fi
-
-# if ! [ -f $CERTS_ ]; then
-#     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-#         -keyout $P_KEY \
-#         -out $CERTS_
-#     echo "Certificate and private key have been generated!"
-# else
-#     echo "There is already a certificate and private key!"
-# fi
 
 sed -i -e "s+CERTS_+$CERTS_+" -e "s+P_KEY+$P_KEY+" /etc/nginx/sites-available/default
 
